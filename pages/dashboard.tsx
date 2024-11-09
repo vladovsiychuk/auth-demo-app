4ee9491b08af92f22914e5e26356f3ceff9666b3
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-11-09 15:59:33 +0200

    create dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-2xl font-semibold">Welcome to the Dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
